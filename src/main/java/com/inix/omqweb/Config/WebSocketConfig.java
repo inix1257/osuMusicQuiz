ef7fe01aceccd72b6b7a426f4512fcccf6db3f7e
inix1257 <inix1257@gmail.com>
2024-09-18 22:18:13 +1000

    comment removal

--- src/main/java/com/inix/omqweb/Config/WebSocketConfig.java
+++ src/main/java/com/inix/omqweb/Config/WebSocketConfig.java
@@ -16,14 +16,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/room");    //해당 주소를 구독하고 있는 클라이언트들에게 메세지 전달
-        registry.setApplicationDestinationPrefixes("/send");       //클라이언트에서 보낸 메세지를 받을 prefix
+        registry.enableSimpleBroker("/room");
+        registry.setApplicationDestinationPrefixes("/send");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp")   //SockJS 연결 주소
-                .setAllowedOrigins(mainUrl)     //모든 주소에서 접근 가능
-                .withSockJS(); //버전 낮은 브라우저에서도 적용 가능
+        registry.addEndpoint("/ws-stomp")
+                .setAllowedOrigins(mainUrl)
+                .withSockJS();
     }
 }
