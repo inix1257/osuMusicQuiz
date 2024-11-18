6df3ff68bef9144d48125f57ab78fc3098bd258b
inix1257 <inix1257@gmail.com>
2024-11-18 19:48:10 +1100

    add daily websocket endpoint

--- src/main/java/com/inix/omqweb/Config/WebSocketConfig.java
+++ src/main/java/com/inix/omqweb/Config/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/room");
+        registry.enableSimpleBroker("/room", "/daily");
         registry.setApplicationDestinationPrefixes("/send");
     }
 
