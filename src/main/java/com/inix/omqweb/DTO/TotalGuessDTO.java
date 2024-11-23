2dae465c9685fb54c5a329b64d0615fd87389e13
inix1257 <inix1257@gmail.com>
2024-11-24 01:36:27 +1100

    added speedpoints for future usage

--- src/main/java/com/inix/omqweb/DTO/TotalGuessDTO.java
+++ src/main/java/com/inix/omqweb/DTO/TotalGuessDTO.java
@@ -2,8 +2,12 @@ package com.inix.omqweb.DTO;
 
 import lombok.Data;
 
+import java.util.concurrent.CopyOnWriteArrayList;
+
 @Data
 public class TotalGuessDTO {
     private int totalGuess;
     private double totalPoints;
+
+    CopyOnWriteArrayList<Double> speedPoints = new CopyOnWriteArrayList<>();
 }
