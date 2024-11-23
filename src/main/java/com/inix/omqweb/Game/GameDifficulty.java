481594c1d64d71176069d20d64d7e8566e35a216
inix1257 <inix1257@gmail.com>
2024-11-24 01:37:07 +1100

    cleanup

--- src/main/java/com/inix/omqweb/Game/GameDifficulty.java
+++ src/main/java/com/inix/omqweb/Game/GameDifficulty.java
@@ -1,8 +1,5 @@
 package com.inix.omqweb.Game;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
 public enum GameDifficulty {
     EASY,
     NORMAL,
