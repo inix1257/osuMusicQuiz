cc034a8fa57ba2ea7b1f2f060c52bfebbf700f6d
inix1257 <inix1257@gmail.com>
2024-11-27 03:34:41 +1100

    add extra difficulty

--- src/main/java/com/inix/omqweb/Util/DifficultyCalc.java
+++ src/main/java/com/inix/omqweb/Util/DifficultyCalc.java
@@ -3,10 +3,11 @@ package com.inix.omqweb.Util;
 import com.inix.omqweb.Game.GameDifficulty;
 
 public class DifficultyCalc {
-    public static double DIFF_EASY_BAR = 0.6;
-    public static double DIFF_NORMAL_BAR = 0.35;
-    public static double DIFF_HARD_BAR = 0.2;
-    public static double DIFF_INSANE_BAR = 0.0;
+    public static double DIFF_EASY_BAR = 0.7;
+    public static double DIFF_NORMAL_BAR = 0.4;
+    public static double DIFF_HARD_BAR = 0.3;
+    public static double DIFF_INSANE_BAR = 0.1;
+    public static double DIFF_EXTRA_BAR = 0.0;
 
     public static GameDifficulty getDifficulty(double guessRate) {
         if (guessRate >= DIFF_EASY_BAR) {
@@ -15,8 +16,10 @@ public class DifficultyCalc {
             return GameDifficulty.NORMAL;
         } else if (guessRate >= DIFF_HARD_BAR) {
             return GameDifficulty.HARD;
-        } else {
+        } else if (guessRate >= DIFF_INSANE_BAR) {
             return GameDifficulty.INSANE;
+        } else {
+            return GameDifficulty.EXTRA;
         }
     }
 }
