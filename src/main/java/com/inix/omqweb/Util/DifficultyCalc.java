121f6bb1991aac9d865a98e0443f387b1c809a0b
inix1257 <inix1257@gmail.com>
2024-12-02 19:24:46 +1100

    new beatmap structure

--- src/main/java/com/inix/omqweb/Util/DifficultyCalc.java
+++ src/main/java/com/inix/omqweb/Util/DifficultyCalc.java
@@ -1,25 +1,22 @@
 package com.inix.omqweb.Util;
 
 import com.inix.omqweb.Game.GameDifficulty;
+import com.inix.omqweb.Game.GuessMode;
 
 public class DifficultyCalc {
-    public static double DIFF_EASY_BAR = 0.7;
-    public static double DIFF_NORMAL_BAR = 0.4;
-    public static double DIFF_HARD_BAR = 0.3;
-    public static double DIFF_INSANE_BAR = 0.1;
-    public static double DIFF_EXTRA_BAR = 0.0;
+    public static double[][] DIFF_BARS = new double[GuessMode.values().length][GameDifficulty.values().length + 1];
 
-    public static GameDifficulty getDifficulty(double guessRate) {
-        if (guessRate >= DIFF_EASY_BAR) {
-            return GameDifficulty.EASY;
-        } else if (guessRate >= DIFF_NORMAL_BAR) {
-            return GameDifficulty.NORMAL;
-        } else if (guessRate >= DIFF_HARD_BAR) {
-            return GameDifficulty.HARD;
-        } else if (guessRate >= DIFF_INSANE_BAR) {
-            return GameDifficulty.INSANE;
-        } else {
+    public static GameDifficulty getDifficulty(GuessMode guessMode, double guessRate) {
+        if (guessRate <= DIFF_BARS[guessMode.getValue()][GameDifficulty.EXTRA.getValue()]) {
             return GameDifficulty.EXTRA;
+        } else if (guessRate <= DIFF_BARS[guessMode.getValue()][GameDifficulty.INSANE.getValue()]) {
+            return GameDifficulty.INSANE;
+        } else if (guessRate <= DIFF_BARS[guessMode.getValue()][GameDifficulty.HARD.getValue()]) {
+            return GameDifficulty.HARD;
+        } else if (guessRate <= DIFF_BARS[guessMode.getValue()][GameDifficulty.NORMAL.getValue()]) {
+            return GameDifficulty.NORMAL;
+        } else {
+            return GameDifficulty.EASY;
         }
     }
 }
