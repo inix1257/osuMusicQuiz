efc5f6440b5a935178fabeadfd6687440992b35a
inix1257 <inix1257@gmail.com>
2024-12-02 19:27:33 +1100

    support other gamemodes

---
+++ src/main/java/com/inix/omqweb/Game/GameMode.java
@@ -0,0 +1,28 @@
+package com.inix.omqweb.Game;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+@Getter
+public enum GameMode {
+    STD(0),
+    TAIKO(1),
+    CTB(2),
+    MANIA(3);
+
+    private final int value;
+
+    GameMode(int value) {
+        this.value = value;
+    }
+
+    public static GameMode fromValue(int value) {
+        for (GameMode mode : GameMode.values()) {
+            if (mode.value == value) {
+                return mode;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant for value: " + value);
+    }
+}
+
