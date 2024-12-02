04fd32fb960e6a533c8e3ef3f978554d5f3d3b6a
inix1257 <inix1257@gmail.com>
2024-12-02 19:27:43 +1100

    cleanup

--- src/main/java/com/inix/omqweb/Game/GameDifficulty.java
+++ src/main/java/com/inix/omqweb/Game/GameDifficulty.java
@@ -1,9 +1,19 @@
 package com.inix.omqweb.Game;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+@Getter
 public enum GameDifficulty {
-    EASY,
-    NORMAL,
-    HARD,
-    INSANE,
-    EXTRA
+    EASY(0),
+    NORMAL(1),
+    HARD(2),
+    INSANE(3),
+    EXTRA(4);
+
+    private final int value;
+
+    GameDifficulty(int value) {
+        this.value = value;
+    }
 }
--- src/main/java/com/inix/omqweb/Game/GuessMode.java
+++ src/main/java/com/inix/omqweb/Game/GuessMode.java
@@ -1,5 +1,18 @@
 package com.inix.omqweb.Game;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+@Getter
 public enum GuessMode {
-    TITLE, PATTERN, ARTIST, CREATOR
+    TITLE(0),
+    ARTIST(1),
+    CREATOR(2),
+    PATTERN(3);
+
+    private final int value;
+
+    GuessMode(int value) {
+        this.value = value;
+    }
 }
