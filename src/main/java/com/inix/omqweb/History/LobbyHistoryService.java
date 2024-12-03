d35bf8092f0405c2b3005b9fc946783a2329a072
inix1257 <inix1257@gmail.com>
2024-12-03 21:25:21 +1100

    add async

--- src/main/java/com/inix/omqweb/History/LobbyHistoryService.java
+++ src/main/java/com/inix/omqweb/History/LobbyHistoryService.java
@@ -2,6 +2,7 @@ package com.inix.omqweb.History;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +17,9 @@ public class LobbyHistoryService {
     private final LobbyHistoryRepository lobbyHistoryRepository;
 
     @PostConstruct
+    @Async
     @Scheduled(fixedDelay = 1000 * 60 * 30) // Cleanup every 30 minutes
-    private void deleteOldHistory() {
+    public void deleteOldHistory() {
         // Delete old history older than 1 week
         LocalDateTime oneWeekAgo = LocalDateTime.now().minusWeeks(1);
         List<LobbyHistory> lobbyHistories = lobbyHistoryRepository.findTop10OldestHistories(oneWeekAgo);
