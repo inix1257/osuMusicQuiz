2e93b3d747b4bfc28b9a43490ef31c5655b608d6
inix1257 <inix1257@gmail.com>
2025-02-27 03:27:35 +0900

    refactor

--- src/main/java/com/inix/omqweb/Discord/DiscordWebhookService.java
+++ src/main/java/com/inix/omqweb/Discord/DiscordWebhookService.java
@@ -1,5 +1,6 @@
 package com.inix.omqweb.Discord;
 
+import com.inix.omqweb.Util.ProfileUtil;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,23 +17,38 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class DiscordWebhookService {
+    private final ProfileUtil profileUtil;
 
-    @Value("${discord.webhook.url}")
-    private String webhookUrl;
+    @Value("${discord.webhook.lobby.url}")
+    private String webhookLobbyUrl;
 
-    public void sendWebhook(String message) {
-        // Send message to Discord webhook
+    @Value("${discord.webhook.beatmap.url}")
+    private String webhookBeatmapUrl;
+
+    public void sendWebhook(String url, String message) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         Map<String, String> payload = new HashMap<>();
-        payload.put("username", "OMQ Log");
+        if (profileUtil.isDevEnv()) {
+            payload.put("username", "OMQ Log (Dev Server)");
+        } else {
+            payload.put("username", "OMQ Log");
+        }
         payload.put("content", message);
 
         RestTemplate restTemplate = new RestTemplate();
 
         HttpEntity<Map<String, String>> entity = new HttpEntity<>(payload, headers);
 
-        restTemplate.exchange(webhookUrl, HttpMethod.POST, entity, String.class);
+        restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+    }
+
+    public void sendLobbyWebhook(String message) {
+        sendWebhook(webhookLobbyUrl, message);
+    }
+
+    public void sendBeatmapWebhook(String message) {
+        sendWebhook(webhookBeatmapUrl, message);
     }
 }
